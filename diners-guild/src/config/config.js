1aa824a0e38650f707aad7f528f73eb3f65d7727
Rachele Edwards <dot.com1472@gmail.com>
2020-12-21 12:06:53 -0700

    updated firebase configs for dev and prod

--- diners-guild/src/config/config.js
+++ diners-guild/src/config/config.js
@@ -1,117 +1,117 @@
-import { lazy } from 'react'
-import locales from './locales'
-import routes from './routes'
-import getMenuItems from './menuItems'
-import themes from './themes'
-import parseLanguages from 'base-shell/lib/utils/locale'
-import grants from './grants'
-import Loading from 'material-ui-shell/lib/components/Loading/Loading'
-import getDefaultRoutes from './getDefaultRoutes'
-import { defaultUserData, isGranted } from 'rmw-shell/lib/utils/auth'
-import { FIREBASE_DEV_APIKEY, FIREBASE_PROD_APIKEY } from '../.env'
+import { lazy } from "react";
+import locales from "./locales";
+import routes from "./routes";
+import getMenuItems from "./menuItems";
+import themes from "./themes";
+import parseLanguages from "base-shell/lib/utils/locale";
+import grants from "./grants";
+import Loading from "material-ui-shell/lib/components/Loading/Loading";
+import getDefaultRoutes from "./getDefaultRoutes";
+import { defaultUserData, isGranted } from "rmw-shell/lib/utils/auth";
+import { FIREBASE_DEV_APIKEY, FIREBASE_PROD_APIKEY } from "../.env";
 
 const config = {
   firebase: {
     prod: {
       initConfig: {
         apiKey: { FIREBASE_PROD_APIKEY },
-        authDomain: 'react-most-wanted-3b1b2.firebaseapp.com',
-        databaseURL: 'https://react-most-wanted-3b1b2.firebaseio.com',
-        projectId: 'dinersguildlive',
-        storageBucket: 'react-most-wanted-3b1b2.appspot.com',
-        messagingSenderId: '258373383650',
-        appId: '1:258373383650:web:b49ad5dd28da999a',
+        authDomain: "dinersguildlive.firebaseapp.com",
+        projectId: "dinersguildlive",
+        storageBucket: "dinersguildlive.appspot.com",
+        messagingSenderId: "252454744274",
+        appId: "1:252454744274:web:ffed196ada2fe5f749fd0b",
+        measurementId: "G-25976HB2WM",
       },
       messaging: {
         publicVapidKey:
-          'BEthk1-Qmoh9opZbi1AUZpxANTu6djVRDph4MLpyO2Qk6Dglm1Sa8Yt_pYi4EhYi3Tj-xgLqUktlbNuP_RP6gto',
+          "BEthk1-Qmoh9opZbi1AUZpxANTu6djVRDph4MLpyO2Qk6Dglm1Sa8Yt_pYi4EhYi3Tj-xgLqUktlbNuP_RP6gto",
       },
     },
     dev: {
       initConfig: {
         apiKey: { FIREBASE_DEV_APIKEY },
-        authDomain: 'react-most-wanted-3b1b2.firebaseapp.com',
-        databaseURL: 'https://react-most-wanted-3b1b2.firebaseio.com',
-        projectId: 'dinersguilddev',
-        storageBucket: 'react-most-wanted-3b1b2.appspot.com',
-        messagingSenderId: '258373383650',
-        appId: '1:258373383650:web:b49ad5dd28da999a',
+        authDomain: "dinersguilddev.firebaseapp.com",
+        projectId: "dinersguilddev",
+        storageBucket: "dinersguilddev.appspot.com",
+        messagingSenderId: "56032715344",
+        appId: "1:56032715344:web:356cee7678ae1678e4e755",
+        measurementId: "G-KTMD83M8JN",
       },
       messaging: {
         publicVapidKey:
-          'BEthk1-Qmoh9opZbi1AUZpxANTu6djVRDph4MLpyO2Qk6Dglm1Sa8Yt_pYi4EhYi3Tj-xgLqUktlbNuP_RP6gto',
+          "BEthk1-Qmoh9opZbi1AUZpxANTu6djVRDph4MLpyO2Qk6Dglm1Sa8Yt_pYi4EhYi3Tj-xgLqUktlbNuP_RP6gto",
       },
     },
     devd: {
       initConfig: {
-        apiKey: 'AIzaSyB31cMH9nJnERC1WCWA7lQHnY08voLs-Z0',
-        authDomain: 'react-most-wanted-dev.firebaseapp.com',
-        databaseURL: 'https://react-most-wanted-dev.firebaseio.com',
-        projectId: 'react-most-wanted-dev',
-        storageBucket: 'react-most-wanted-dev.appspot.com',
-        messagingSenderId: '70650394824',
-        appId: '1:70650394824:web:7cd3113c37741efc',
+        apiKey: "AIzaSyB31cMH9nJnERC1WCWA7lQHnY08voLs-Z0",
+        authDomain: "react-most-wanted-dev.firebaseapp.com",
+        databaseURL: "https://react-most-wanted-dev.firebaseio.com",
+        projectId: "react-most-wanted-dev",
+        storageBucket: "react-most-wanted-dev.appspot.com",
+        messagingSenderId: "70650394824",
+        appId: "1:70650394824:web:7cd3113c37741efc",
       },
       messaging: {
         publicVapidKey:
-          'BGddXH_O6qLmcingsSJx-R3hC8U9yUr2mW4ko63fF__e50WvfRcBfZu_JyBzLI35DNUE5x_9CPBqe64BWniCxV0',
+          "BGddXH_O6qLmcingsSJx-R3hC8U9yUr2mW4ko63fF__e50WvfRcBfZu_JyBzLI35DNUE5x_9CPBqe64BWniCxV0",
       },
     },
     firebaseuiProps: {
       signInOptions: [
-        'google.com',
-        'facebook.com',
-        'twitter.com',
-        'github.com',
-        'password',
-        'phone',
+        "google.com",
+        "facebook.com",
+        "twitter.com",
+        "github.com",
+        "password",
+        "phone",
       ],
     },
   },
   googleMaps: {
-    apiKey: 'AIzaSyByMSTTLt1Mf_4K1J9necAbw2NPDu2WD7g',
+    apiKey: "AIzaSyByMSTTLt1Mf_4K1J9necAbw2NPDu2WD7g",
   },
   auth: {
     grants,
-    redirectTo: '/dashboard',
-    persistKey: 'base-shell:auth',
-    signInURL: '/signin',
+    redirectTo: "/dashboard",
+    persistKey: "base-shell:auth",
+    signInURL: "/signin",
     onAuthStateChanged: async (user, auth, firebaseApp) => {
       if (user != null) {
         const grantsSnap = await firebaseApp
           .database()
           .ref(`user_grants/${user.uid}`)
-          .once('value')
+          .once("value");
         const notifcationsDisabledSnap = await firebaseApp
           .database()
           .ref(`disable_notifications/${user.uid}`)
-          .once('value')
+          .once("value");
 
         const isAdminSnap = await firebaseApp
           .database()
           .ref(`admins/${user.uid}`)
-          .once('value')
+          .once("value");
 
         firebaseApp
           .database()
           .ref(`user_grants/${user.uid}`)
-          .on('value', (snap) => {
-            auth.updateAuth({ grants: snap.val() })
-          })
+          .on("value", (snap) => {
+            auth.updateAuth({ grants: snap.val() });
+          });
 
         firebaseApp
           .database()
           .ref(`disable_notifications/${user.uid}`)
-          .on('value', (snap) => {
-            auth.updateAuth({ notificationsDisabled: !!snap.val() })
-          })
+          .on("value", (snap) => {
+            auth.updateAuth({ notificationsDisabled: !!snap.val() });
+          });
 
         firebaseApp
           .database()
           .ref(`admins/${user.uid}`)
-          .on('value', (snap) => {
-            auth.updateAuth({ isAdmin: !!snap.val() })
-          })
+          .on("value", (snap) => {
+            auth.updateAuth({ isAdmin: !!snap.val() });
+          });
 
         auth.updateAuth({
           ...defaultUserData(user),
@@ -119,7 +119,7 @@ const config = {
           notificationsDisabled: notifcationsDisabledSnap.val(),
           isAdmin: !!isAdminSnap.val(),
           isGranted,
-        })
+        });
 
         firebaseApp.database().ref(`users/${user.uid}`).update({
           displayName: user.displayName,
@@ -129,19 +129,19 @@ const config = {
           emailVerified: user.emailVerified,
           isAnonymous: user.isAnonymous,
           notificationsDisabled: notifcationsDisabledSnap.val(),
-        })
+        });
 
         await firebaseApp
           .database()
           .ref(`user_chats/${user.uid}/public_chat`)
           .update({
-            displayName: 'Public Chat',
-            lastMessage: 'Group chat',
+            displayName: "Public Chat",
+            lastMessage: "Group chat",
             path: `group_chat_messages/public_chat`,
-          })
+          });
       } else {
-        firebaseApp.database().ref().off()
-        auth.setAuth(defaultUserData(user))
+        firebaseApp.database().ref().off();
+        auth.setAuth(defaultUserData(user));
       }
     },
   },
@@ -149,44 +149,44 @@ const config = {
   routes,
   locale: {
     locales,
-    persistKey: 'base-shell:locale',
-    defaultLocale: parseLanguages(['en', 'de', 'ru'], 'en'),
+    persistKey: "base-shell:locale",
+    defaultLocale: parseLanguages(["en", "de", "ru"], "en"),
     onError: (e) => {
       //console.warn(e)
 
-      return
+      return;
     },
   },
   menu: {
     getMenuItems,
     MenuHeader: lazy(() =>
-      import('material-ui-shell/lib/components/MenuHeader/MenuHeader')
+      import("material-ui-shell/lib/components/MenuHeader/MenuHeader")
     ),
   },
   theme: {
     themes,
-    defaultThemeID: 'default',
-    defaultType: 'light',
+    defaultThemeID: "default",
+    defaultType: "light",
   },
   pages: {
-    LandingPage: lazy(() => import('../pages/LandingPage')),
-    PageNotFound: lazy(() => import('../pages/PageNotFound')),
+    LandingPage: lazy(() => import("../pages/LandingPage")),
+    PageNotFound: lazy(() => import("../pages/PageNotFound")),
   },
   components: {
     Menu: lazy(() =>
-      import('rmw-shell/lib/containers/FirebaseMenu/FirebaseMenu')
+      import("rmw-shell/lib/containers/FirebaseMenu/FirebaseMenu")
     ),
     Loading,
   },
 
   containers: {
     AppContainer: lazy(() =>
-      import('material-ui-shell/lib/containers/AppContainer/AppContainer')
+      import("material-ui-shell/lib/containers/AppContainer/AppContainer")
     ),
     LayoutContainer: lazy(() =>
-      import('rmw-shell/lib/containers/LayoutContainer/LayoutContainer')
+      import("rmw-shell/lib/containers/LayoutContainer/LayoutContainer")
     ),
   },
-}
+};
 
-export default config
+export default config;
