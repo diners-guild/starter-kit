2ae02b8033ae17cb8ea357e443d1ceb50764eea9
1BJax <>
2020-12-23 14:02:54 -0700

    remove firebase pages

--- diners-guild/src/config/routes.js
+++ diners-guild/src/config/routes.js
@@ -11,16 +11,10 @@ const Coupons = lazy(() => import('../pages/Coupons'))
 const Coupon = lazy(() => import('../pages/Coupons/Coupon'))
 const Tasks = lazy(() => import('../pages/Demo/Tasks'))
 const Task = lazy(() => import('../pages/Demo/Tasks/Task'))
-const FirebaseCols = lazy(() => import('../pages/Firebase/Cols'))
-const FirebaseDocs = lazy(() => import('../pages/Firebase/Docs'))
-const FirebaseLists = lazy(() => import('../pages/Firebase/Lists'))
-const FirebaseMessaging = lazy(() => import('../pages/Firebase/Messaging'))
-const FirebasePaths = lazy(() => import('../pages/Firebase/Paths'))
-const FirebaseStorage = lazy(() => import('../pages/Firebase/Storage'))
 const Dashboard = lazy(() => import('../pages/Dashboard'))
-const GettingStarted = lazy(() =>
-  import('../pages/Documentation/GettingStarted')
-)
+// const GettingStarted = lazy(() =>
+//   import('../pages/Documentation/GettingStarted')
+// )
 
 const routes = [
   <Route path="/about" exact component={About} />,
@@ -28,10 +22,6 @@ const routes = [
   <AuthorizedRoute path="/coupons/:uid" exact component={Coupon}/>,
   <AuthorizedRoute path="/create_coupon" exact component={Coupon} />,
   <AuthorizedRoute path="/dashboard" exact component={Dashboard} />,
-  <AuthorizedRoute path="/firebase_paths" exact component={FirebasePaths} />,
-  <AuthorizedRoute path="/firebase_lists" exact component={FirebaseLists} />,
-  <AuthorizedRoute path="/firebase_docs" exact component={FirebaseDocs} />,
-  <AuthorizedRoute path="/firebase_cols" exact component={FirebaseCols} />,
   <AuthorizedRoute path="/admin" exact component={Admin} />,
   <AuthorizedRoute path="/companies" exact component={Companies} />,
   <AuthorizedRoute path="/companies/:uid" exact component={Company} />,
@@ -39,16 +29,6 @@ const routes = [
   <AuthorizedRoute path="/tasks" exact component={Tasks} />,
   <AuthorizedRoute path="/tasks/:uid" exact component={Task} />,
   <AuthorizedRoute path="/create_task" exact component={Task} />,
-  <AuthorizedRoute
-    path="/firebase_messaging"
-    exact
-    component={FirebaseMessaging}
-  />,
-  <AuthorizedRoute
-    path="/firebase_storage"
-    exact
-    component={FirebaseStorage}
-  />,
 ]
 
 export default routes
