7e892659f693b6543fe9b9ea5703ec21761c5ce6
Rachele Edwards <dot.com1472@gmail.com>
2020-12-29 12:02:54 -0700

    deleted VirtualCard.js and just modeled index.js similarly to other wallet components

--- diners-guild/src/pages/Wallet/VirtualCards/index.js
+++ diners-guild/src/pages/Wallet/VirtualCards/index.js
@@ -1,85 +1,20 @@
-import Avatar from '@material-ui/core/Avatar'
-import Assignment from '@material-ui/icons/Assignment'
-import Divider from '@material-ui/core/Divider'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemAvatar from '@material-ui/core/ListItemAvatar'
-import ListItemText from '@material-ui/core/ListItemText'
-import React, { useCallback } from 'react'
-import { ListPage } from 'rmw-shell/lib/containers/Page'
-import { useHistory } from 'react-router-dom'
+import React from 'react'
+import Page from 'material-ui-shell/lib/containers/Page/Page'
 import { useIntl } from 'react-intl'
-import { useFirebase } from 'rmw-shell/lib/providers/Firebase'
 
-const path = 'tasks'
+const VirtualCards = () => {
+    const intl = useIntl()
 
-const fields = [
-  {
-    name: 'name',
-    label: 'Name',
-  },
-  {
-    name: 'description',
-    label: 'Description',
-  },
-]
-
-const Row = ({ data, index, style }) => {
-  const { title = '', helper = {}, key } = data
-  const history = useHistory()
-
-  return (
-    <div key={key} style={style}>
-      <ListItem
-        button
-        alignItems="flex-start"
-        style={{ height: 72 }}
-        onClick={() => {
-          history.push(`${path}/${key}`)
-        }}
-      >
-        <ListItemAvatar>
-          <Avatar>
-            <Assignment />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary={title} secondary={helper?.label} />
-      </ListItem>
-      <Divider variant="inset" />
-    </div>
-  )
+    return (
+        <Page
+            pageTitle={intl.formatMessage({
+            id: 'virtual_cards',
+            defaultMessage: 'Virtual Cards',
+            })}
+        >
+            <h1>&nbsp;&nbsp;&nbsp;This is showing your Virtual Card information!</h1>
+        </Page>
+    )
 }
 
-const Tasks = () => {
-  const intl = useIntl()
-  const history = useHistory()
-  const { firebaseApp } = useFirebase()
-
-  const getRef = useCallback(() => {
-    return firebaseApp.database().ref(`public_tasks`).limitToLast(50)
-  }, [firebaseApp])
-
-  return (
-    <ListPage
-      reverse
-      fields={fields}
-      path={'public_tasks'}
-      getRef={getRef}
-      createGrant="create_task"
-      Row={Row}
-      listProps={{ itemSize: 72 }}
-      getPageProps={() => {
-        return {
-          pageTitle: intl.formatMessage({
-            id: path,
-            defaultMessage: 'Tasks',
-          }),
-        }
-      }}
-      onCreateClick={() => {
-        history.push('/create_task')
-      }}
-    />
-  )
-}
-
-export default Tasks
+export default VirtualCards
