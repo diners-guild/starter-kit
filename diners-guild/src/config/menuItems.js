e0a03f35688029e378f8928c068f4b558e7f1329
1BJax <>
2020-12-29 15:25:04 -0700

    remove admin link from wallet

--- diners-guild/src/config/menuItems.js
+++ diners-guild/src/config/menuItems.js
@@ -156,15 +156,6 @@ const getMenuItems = (props) => {
       leftIcon: <CreditCardIcon />,
       nestedItems: [
         {
-          value: '/admin',
-          visible: !isAdmin,
-          primaryText: intl.formatMessage({
-            id: 'admin',
-            defaultMessage: 'Admin',
-          }),
-          leftIcon: <Security />,
-        },
-        {
           value: '/virtual_cards',
           visible: isGranted(auth, 'read_companies'),
           primaryText: intl.formatMessage({
