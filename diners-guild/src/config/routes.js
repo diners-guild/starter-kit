c95d058a5bf6dee819b511668d8280d865c54ed4
Rachele Edwards <dot.com1472@gmail.com>
2020-12-29 11:54:05 -0700

    added routes and imports for transactions, physical and virtual cards

--- diners-guild/src/config/routes.js
+++ diners-guild/src/config/routes.js
@@ -9,23 +9,31 @@ const Companies = lazy(() => import('../pages/Demo/Companies'))
 const Company = lazy(() => import('../pages/Demo/Companies/Company'))
 const Coupons = lazy(() => import('../pages/Coupons'))
 const Coupon = lazy(() => import('../pages/Coupons/Coupon'))
+const Dashboard = lazy(() => import('../pages/Dashboard'))
+const PhysicalCards = lazy(() => import('../pages/Wallet/PhysicalCards'))
 const Tasks = lazy(() => import('../pages/Demo/Tasks'))
 const Task = lazy(() => import('../pages/Demo/Tasks/Task'))
-const Dashboard = lazy(() => import('../pages/Dashboard'))
+const Transactions = lazy(() => import('../pages/Wallet/Transactions'))
+const VirtualCards = lazy(() => import('../pages/Wallet/VirtualCards'))
+
 
 const routes = [
   <Route path="/about" exact component={About} />,
   <AuthorizedRoute path="/coupons" exact component={Coupons}/>,
   <AuthorizedRoute path="/coupons/:uid" exact component={Coupon}/>,
   <AuthorizedRoute path="/create_coupon" exact component={Coupon} />,
-  <AuthorizedRoute path="/dashboard" exact component={Dashboard} />,
   <AuthorizedRoute path="/admin" exact component={Admin} />,
   <AuthorizedRoute path="/companies" exact component={Companies} />,
   <AuthorizedRoute path="/companies/:uid" exact component={Company} />,
   <AuthorizedRoute path="/create_company" exact component={Company} />,
+  <AuthorizedRoute path="/dashboard" exact component={Dashboard} />,
+  <AuthorizedRoute path="/physical_cards" exact component={PhysicalCards} />,
   <AuthorizedRoute path="/tasks" exact component={Tasks} />,
   <AuthorizedRoute path="/tasks/:uid" exact component={Task} />,
   <AuthorizedRoute path="/create_task" exact component={Task} />,
+  <AuthorizedRoute path="/transactions" exact component={Transactions} />,
+  <AuthorizedRoute path="/virtual_cards" exact component={VirtualCards} />,
+
 ]
 
 export default routes
